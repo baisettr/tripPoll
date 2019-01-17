d56048050acb249299b75d50122909503c79c78f
rk <ramakrishna8051@gmail.com>
2019-01-16 23:29:13 -0800

    Adding to the source

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import './App.css';
+import Place from './containers/Place';
+import Options from './containers/Options';
+import Propose from './containers/Propose';
+import Select from './containers/Select';
+import View from './containers/View';
+import NavBar from './containers/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Place} />
+          <Route path="/options" component={Options} />
+          <Route path="/propose" component={Propose} />
+          <Route path="/select" component={Select} />
+          <Route path="/view" component={View} />
+        </Switch >
+      </div>
+    );
+  }
+}
+
+export default App;
