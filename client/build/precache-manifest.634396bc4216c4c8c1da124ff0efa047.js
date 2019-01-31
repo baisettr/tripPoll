d27424448ed202bf59e021b913b95e66120521f8
rk <ramakrishna8051@gmail.com>
2019-01-31 11:02:47 -0800

    resolving url issues

---
+++ client/build/precache-manifest.634396bc4216c4c8c1da124ff0efa047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e94c69e33df0ac1f2925",
+    "url": "/trpo/static/js/runtime~main.e94c69e3.js"
+  },
+  {
+    "revision": "a153d35d51f6b524bd4a",
+    "url": "/trpo/static/js/main.a153d35d.chunk.js"
+  },
+  {
+    "revision": "2dfe13a6d5f5e07678bd",
+    "url": "/trpo/static/js/1.2dfe13a6.chunk.js"
+  },
+  {
+    "revision": "a153d35d51f6b524bd4a",
+    "url": "/trpo/static/css/main.09bb8b51.chunk.css"
+  },
+  {
+    "revision": "2dfe13a6d5f5e07678bd",
+    "url": "/trpo/static/css/1.3625524c.chunk.css"
+  },
+  {
+    "revision": "5d066659b3c23d1efab68b5a1db478f8",
+    "url": "/trpo/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2322e015876c2f142492424fe33c9b23.js"
+  "/trpo/precache-manifest.634396bc4216c4c8c1da124ff0efa047.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/trpo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
