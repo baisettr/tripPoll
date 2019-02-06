280153eaa6f0d1a58f395b5c8b96e8d0f89e9ff0
rk <ramakrishna8051@gmail.com>
2019-02-06 05:34:40 -0800

    guarding the routes and refactoring div to react fragments, code splitting via react lazy loading with suspense

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component, Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Home from './containers/Home';
 import Options from './containers/Options';
@@ -9,20 +9,24 @@ import View from './containers/View';
 import NavBar from './containers/Nav';
 import Login from './containers/Login';
 
+const Login = React.lazy(() => import('./containers/Login'));
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <NavBar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
           <Route path="/options" component={Options} />
           <Route path="/propose" component={Propose} />
           <Route path="/select" component={Select} />
           <Route path="/view" component={View} />
+          <Route path="/login" render={() => (<Suspense fallback={<div>Loading...</div>}><Login /></Suspense>)} />
+          {/* <Route render={() => <h1>404 Authorizing an invalid url!</h1>} /> */}
+          <Redirect from="/" to="/login" />
         </Switch >
-      </div>
+      </React.Fragment>
     );
   }
 }
