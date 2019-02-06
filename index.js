001899a570e4d918d5025bbe46a41e27ca936bf9
rk <ramakrishna8051@gmail.com>
2019-02-06 05:35:24 -0800

    refactoring the server side

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./routes/auth')(app);
+require('./routes/places')(app);
+require('./routes/trip')(app);
+
+if (process.env.NOD_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
