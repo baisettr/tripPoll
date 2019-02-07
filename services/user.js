b70ad3f067a40d8fdc025a246c7454705c97bc9c
rk <ramakrishna8051@gmail.com>
2019-02-07 14:28:11 -0800

    defining user routes and suceesful tested jwt

--- services/user.js
+++ services/user.js
@@ -11,7 +11,8 @@ function userSignUp(userDetails) {
                 console.log(res.data);
                 if (res.data) {
                     const user = { userId: res.data.userId, userOId: res.data._id.$oid }
-                    getJwtToken(user).then((e) => resolve(e));
+                    const token = jwt.sign(user, keys.jwtKey, { algorithm: 'HS256', expiresIn: 60 * 60 });
+                    resolve(token);
                 } else {
                     reject("Invalid registration details!");
                 }
@@ -34,7 +35,8 @@ function userSignIn(userDetails) {
                     if (userFullDetails.password === userDetails.password) {
                         console.log("success");
                         const user = { userId: userFullDetails.userId, userOId: userFullDetails._id.$oid }
-                        getJwtToken(user).then((e) => resolve(e));
+                        const token = jwt.sign(user, keys.jwtKey, { algorithm: 'HS256', expiresIn: 60 * 60 });
+                        resolve(token);
                     } else {
                         reject("Incorrect password. Please try again!");
                     };
@@ -48,28 +50,51 @@ function userSignIn(userDetails) {
     });
 }
 
+function userDetails(userDetails) {
+    return new Promise((resolve, reject) => {
+        const userOId = userDetails.userOId;
+        const url = 'https://api.mlab.com/api/1/databases/tripo/collections/users/' + userOId + '?apiKey=' + keys.mlabAPIKey;
+        axios.get(url)
+            .then((res) => {
+                console.log(res.data);
+                const userFullDetails = res.data;
+                if (userFullDetails.length !== 0) {
+                    console.log("success");
+                    const user = { userName: userFullDetails.userName, userEmail: userFullDetails.userEmail }
+                    resolve(user);
+                } else {
+                    reject("Invalid User. Please try again!");
+                };
+            })
+            .catch((err) => {
+                reject("Internal error. Please try again!");
+            });
+    });
+}
+
+module.exports = { userSignUp, userSignIn, userDetails };
+
+
+
+
+
+/* let userSIgnUpDetails = { userId: 2345, password: 'gbhnjnjjn7', userName: 'hello', userEmail: 'cfvghb@fgh.com' };
+let userSIgnInDetails = { userEmail: "cfvghb@fgh.com", password: 'gbhnjnjjn7' };
+userSignUp(userSIgnUpDetails).then((e) => console.log(e));
+userSignIn(userSIgnInDetails).then((e) => console.log(e));
+
 function getJwtToken(user) {
     return new Promise((resolve, reject) => {
         jwt.sign(user, keys.jwtKey, { algorithm: 'HS256', expiresIn: 60 * 60 }, function (err, token) {
             resolve(token);
         });
     });
-}
+};
 
-function isUserValid(token) {
+function verifyJwtToken(token) {
     return new Promise((resolve, reject) => {
         jwt.verify(token, keys.jwtKey, { algorithm: 'HS256' }, function (err, user) {
             resolve(user);
         });
     });
-}
-
-module.exports = { userSignUp, userSignIn, isUserValid };
-
-
-/* let userSIgnUpDetails = { userId: 2345, password: 'gbhnjnjjn7', userName: 'hello', userEmail: 'cfvghb@fgh.com' };
-let userSIgnInDetails = { userEmail: "cfvghb@fgh.com", password: 'gbhnjnjjn7' };
-//userSignUp(userSIgnUpDetails).then((e) => console.log(e));
-userSignIn(userSIgnInDetails).then((e) => console.log(e));
-//let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInVzZXJPSWQiOiI1YzVhNDAyZTVkMGU2NTE0ZDIxYjYxMGEiLCJpYXQiOjE1NDk0MTg1NDIsImV4cCI6MTU0OTQxODYwMn0.KS-wVNbft5u4Dm0jhBKP5KnYBQfm2SUjQ70R882OOfo'
-//verifyJwtToken(token).then((e) => console.log(e)); */
+}; */
