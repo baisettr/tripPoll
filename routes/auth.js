3f27344a33c4906a47fd686c522db448d3fe519d
rk <ramakrishna8051@gmail.com>
2019-02-07 14:29:27 -0800

    validating the user routes and defining validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,37 @@
-const axios = require('axios');
-const jwt = require('jsonwebtoken');
 const auth = require('../services/user');
+const checkUserAuth = require('../services/auth');
 
 module.exports = app => {
     app.post('/userSignUp', (req, res) => {
         const userDetails = req.body.userDetails;
         auth.userSignUp(userDetails)
-            .then((data) => {
-                res.send(data);
+            .then((token) => {
+                res.status(200).json({ token });
             })
             .catch((err) => {
-                console.log(err)
+                res.status(400).json({ message: err });
             });
     });
 
     app.post('/userSignIn', (req, res) => {
         const userDetails = req.body.userDetails;
         auth.userSignIn(userDetails)
-            .then((data) => {
-                res.send(data);
+            .then((token) => {
+                res.status(200).json({ token });
             })
             .catch((err) => {
-                console.log(err)
+                res.status(400).json({ message: err });
             });
     });
 
-    app.get('/userDetails', auth.isUserValid, (req, res) => {
-        const userDetails = req.query.userDetails;
-        auth.getUserDetails(userDetails)
+    app.get('/userDetails', checkUserAuth, (req, res) => {
+        const user = req.user;
+        auth.userDetails(user)
             .then((data) => {
-                res.send(data);
+                res.status(200).json({ data });
             })
             .catch((err) => {
-                console.log(err)
+                res.status(400).json({ message: err });
             });
     });
 };
