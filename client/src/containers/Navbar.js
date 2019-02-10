5bceba81a2aea531d95d0099173db0b5c09942a3
rk <ramakrishna8051@gmail.com>
2019-02-10 09:12:58 -0800

    react bootstrap navbar with react dom Links for handling page refresh

--- client/src/containers/Navbar.js
+++ client/src/containers/Navbar.js
@@ -1,54 +1,34 @@
 import React from 'react';
-/* import { Navbar, NavItem, Nav, NavDropdown, MenuItem } from 'react-bootstrap'; */
-import { IconButton, Badge } from '@material-ui/core';
-import { AccountCircle } from '@material-ui/icons';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, } from 'react-bootstrap';
 
-const users = [{ id: 1, status: 0 }, { id: 2, status: 0 }, { id: 3, status: 0 }, { id: 4, status: 0 }];
+const NavBarComponent = (props) => {
+    //console.log(props);
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <div className="container" >
+                <Link className="navbar-brand" to="/">Trip Poll</Link>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Link className="nav-link" to="/propose">Propose</Link>
+                        <Link className="nav-link" to="/view">View</Link>
+                        <Link className="nav-link" to="/select">Select Options</Link>
+                        <NavDropdown title="Trips" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        {props.status ? <Link className="nav-link" to="/logout">Logout</Link> : <Link className="nav-link" to="/login">Login</Link>}
+                        <Link className="nav-link" to="/view">Contact</Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
+        </Navbar>)
+}
 
-const UsersTab = () => users.map(a => <MenuItem key={a.id}>{a.id + "  " + a.status}</MenuItem>)
-
-const NavBar = () =>
-    <Navbar inverse collapseOnSelect>
-        <Navbar.Header>
-            <Navbar.Brand><a href="/">Trip Poll</a></Navbar.Brand>
-            <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-            <Nav>
-                <NavItem eventKey={1} href="/propose">Propose a Trip</NavItem>
-                <NavItem eventKey={2} href="/view">View a Trip</NavItem>
-                <NavDropdown eventKey={3} title="Trips" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1} href="/trips">My Trips</MenuItem>
-                    <MenuItem eventKey={3.2} href="/places">Friend's Trips</MenuItem>
-                    <MenuItem eventKey={3.3} href="/updates">Trip Updates</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.3} href="/info">Information</MenuItem>
-                </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-                <NavDropdown eventKey={3} title="Notifications" id="basic-nav-dropdown">
-                    <UsersTab />
-                </NavDropdown>
-                <NavItem eventKey={1} href="/contact">Contact</NavItem>
-                <NavItem eventKey={2} href="/login">Login</NavItem>
-                <NavItem><img src="" /></NavItem>
-                {/* <NavItem eventKey={2} href="/login">
-                    <Badge badgeContent={4} color="secondary">
-                        <MailIcon />
-                    </Badge>
-                </NavItem>
-                <NavItem eventKey={2} href="/login">
-                    <Badge badgeContent={17} color="secondary">
-                        <NotificationsIcon />
-                    </Badge>
-                </NavItem>
-                <NavItem eventKey={2} href="/login">
-                    <AccountCircle />
-                </NavItem> */}
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-
-export default NavBar;
+export default NavBarComponent;
