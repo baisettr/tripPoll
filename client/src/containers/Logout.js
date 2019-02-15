d46a063c332f477ee1d3eafcefd5d8824400128f
rk <ramakrishna8051@gmail.com>
2019-02-15 08:24:29 -0800

    creating a logout class component to handle redirecting

--- client/src/containers/Logout.js
+++ client/src/containers/Logout.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect, withRouter } from 'react-router-dom';
 
-const Logout = (props) => {
-    userSignOut(props);
-    return (<div></div>)
+class Logout extends Component {
+    constructor(props) {
+        super(props);
+        this.userSignOut();
+    }
+    userSignOut = async () => {
+        localStorage.removeItem('userExpiration');
+        localStorage.removeItem('userToken');
+        this.props.logoutChange(false);
+    };
+    render() {
+        return (<Redirect from="/logout" to="/" />)
+    }
 }
 
-const userSignOut = async (props) => {
-    localStorage.removeItem('userExpiration');
-    localStorage.removeItem('userToken');
-    props.logoutChange(false);
-};
-
-export default Logout;
+export default withRouter(Logout);
