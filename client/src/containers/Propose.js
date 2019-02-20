06f0b206a5e7507d6097cf6969dfee15070bba37
rk <ramakrishna8051@gmail.com>
2019-02-20 10:10:19 -0800

    adding progress bar saving trip to user trips, social sharing options

--- client/src/containers/Propose.js
+++ client/src/containers/Propose.js
@@ -5,6 +5,10 @@ import 'react-day-picker/lib/style.css';
 import AirbnbTest from '../components/AirbnbComponent';
 import GoogleTest from '../components/GoogleComponent';
 import { withRouter, Link } from 'react-router-dom';
+import {
+    FacebookShareButton, TwitterShareButton, WhatsappShareButton, EmailShareButton,
+    EmailIcon, WhatsappIcon, TwitterIcon, FacebookIcon
+} from 'react-share';
 
 const styles = {
     divHome: {
@@ -17,7 +21,9 @@ const styles = {
 class Propose extends Component {
     constructor(props) {
         super(props);
-        this.state = { userId: "8907", activeStep: 0, destination: "", suggestedPlaces: [], selectedDays: [], listAirbnbPlaces: [], listGooglePlaces: [], selectedGooglePlaces: {}, selectedAirbnbPlaces: {}, userOtherOptions: { userShare: 0, userHasCar: false, userCarFit: 0 } };
+        const userId = localStorage.getItem('userId');
+        const userName = localStorage.getItem('userName');
+        this.state = { user: { userId, userName }, activeStep: 0, destination: "", width: 20, tripSelectionUrl: "https://rkbeavs.me/trpo/select?tripId=", suggestedPlaces: ['New York, NY, USA', 'San Francisco, CA, USA', 'Florida, USA', 'Portland, OR, USA'], selectedDays: [], listAirbnbPlaces: [], listGooglePlaces: [], selectedGooglePlaces: {}, selectedAirbnbPlaces: {}, userOtherOptions: { userShare: 0, userHasCar: false, userCarFit: 0 } };
     }
 
     getPlaceSuggestions = (enteredPlace) => {
@@ -37,7 +43,7 @@ class Propose extends Component {
         <div>
             <h4>Propose a City for a Trip</h4>
             <br />
-            <input className="inputPlace" style={styles.inputPlace} placeholder="Enter a destination" required={true} type="text" defaultValue={this.state.destination}
+            <input className="form-control" style={{ width: '50%', margin: "auto" }} placeholder="Enter a destination" required={true} type="text" defaultValue={this.state.destination}
                 list="data" onChange={(e) => { this.getPlaceSuggestions(e.target.value); this.setState({ destination: e.target.value }) }} />
             <datalist id="data">
                 {this.state.suggestedPlaces.map((item) =>
@@ -88,25 +94,15 @@ class Propose extends Component {
     generateNewTrip = () => {
         const tripId = Math.round(10000 + Math.random() * 10000, 5);
         const userSelection = {};
-        const userId = this.state.userId;
-        /* const selectedGooglePlaces = this.state.selectedGooglePlaces;
-        const selectedAirbnbPlaces = this.state.selectedAirbnbPlaces;
-        let userSelectedGooglePlaces = [];
-        let userSelectedAirbnbPlaces = [];
-        for (place in selectedGooglePlaces) {
-            userSelectedGooglePlaces = [...selectedGooglePlaces[place]]
-        }
-        for (place in selectedAirbnbPlaces) {
-            userSelectedAirbnbPlaces = [...selectedAirbnbPlaces[place]]
-        } */
-        userSelection[userId] = { selectedGooglePlaces: this.state.selectedGooglePlaces, selectedAirbnbPlaces: this.state.selectedAirbnbPlaces, selectedDays: this.state.selectedDays, userOtherOptions: this.state.userOtherOptions };
+        const userId = this.state.user.userId;
+        userSelection[userId] = { userName: this.state.user.userName, selectedGooglePlaces: this.state.selectedGooglePlaces, selectedAirbnbPlaces: this.state.selectedAirbnbPlaces, selectedDays: this.state.selectedDays, userOtherOptions: this.state.userOtherOptions };
 
-        const trip = { tripId: tripId, tripDestination: this.state.destination, tripListGooglePlaces: this.state.listGooglePlaces, tripOwnerId: this.state.userId, tripSelectedDays: this.state.selectedDays, tripListAirbnbPlaces: this.state.listAirbnbPlaces, tripSelectedOptions: userSelection };
-        const tripSelectionUrl = '/select?tripId=' + tripId;
-        const tripOptionsUrl = '/options?tripId=' + tripId;
-        this.setState({ tripId, tripSelectionUrl, tripOptionsUrl });
+        const trip = { tripId: tripId, tripDestination: this.state.destination, tripListGooglePlaces: this.state.listGooglePlaces, tripOwner: this.state.user, tripSelectedDays: this.state.selectedDays, tripListAirbnbPlaces: this.state.listAirbnbPlaces, tripSelectedOptions: userSelection };
+        const tripSelectionUrl = this.state.tripSelectionUrl + tripId;
+        this.setState({ tripId, tripSelectionUrl });
         //console.log(trip);
         this.saveNewTrip(trip);
+        this.saveUserNewTrip(tripId, this.state.destination)
     }
 
     saveNewTrip = (trip) => {
@@ -116,6 +112,20 @@ class Propose extends Component {
         axios.post(url, { data: trip }, { headers }
         ).then((res) => {
             console.log("Saved");
+            console.log(res.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    saveUserNewTrip = async (tripId, tripDestination) => {
+        const url = '/userNewTrip';
+        const newTrip = { tripId, tripDestination };
+        const userToken = localStorage.userToken;
+        const headers = { Authorization: 'Bearer ' + userToken };
+        axios.post(url, { data: newTrip }, { headers }
+        ).then((res) => {
+            console.log("User Trip Saved");
             console.log(res.data);
         }).catch((error) => {
             console.log(error);
@@ -158,9 +168,9 @@ class Propose extends Component {
             <div >
                 <label style={{ paddingRight: '15px' }}>Do you own a Car for the Trip?</label>
                 <label>
-                    <input type="radio" value="yes" onClick={(e) => {
+                    <input type="checkbox" checked={this.state.userOtherOptions.userHasCar} onChange={(e) => {
                         const { userOtherOptions } = this.state;
-                        userOtherOptions.userHasCar = true;
+                        userOtherOptions.userHasCar = !this.state.userOtherOptions.userHasCar;
                         this.setState({ userOtherOptions });
                     }} /><span style={{ paddingLeft: '5px' }}>Yes</span>
                 </label>
@@ -179,24 +189,59 @@ class Propose extends Component {
 
         </div>
 
-    DisplayLinks = () => <div>
-        <div>
-            <label>Link to Share</label>
-            <input className="inputPlace" defaultValue={'http://localhost:3000' + this.state.tripSelectionUrl} readOnly />
-            <button className="btn btn-link" onClick={() => this.props.history.push(this.state.tripSelectionUrl)}> Click to Preview</button>
+    DisplayLinks = () =>
+        <div >
+            <div style={{ display: "inline-flex" }}>
+                <div className="btn">
+                    <FacebookShareButton className="btn" url={this.state.tripSelectionUrl} quote={"Please select trip options for " + this.state.destination} >
+                        <FacebookIcon size={40} round={true} />
+                    </FacebookShareButton>
+                    <h6>Facebook</h6>
+                </div>
+                <div className="btn">
+                    <TwitterShareButton className="btn" hashtags={['trip poll']} title={"Please select trip options for " + this.state.destination} url={this.state.tripSelectionUrl} >
+                        <TwitterIcon size={40} round={true} />
+                    </TwitterShareButton>
+                    <h6>Twitter</h6>
+                </div>
+                <div className="btn">
+                    <EmailShareButton className="btn" url={this.state.tripSelectionUrl} subject={"Please select trip options for " + this.state.destination} body={"Hello\n\nWelcome to Trip Poll!\nPlease follow the link to select options\n" + this.state.tripSelectionUrl + "\n\nThank You\n" + this.state.user.userName}>
+                        <EmailIcon size={40} round={true} />
+                    </EmailShareButton>
+                    <h6>Email</h6>
+                </div>
+                <div className="btn">
+                    <WhatsappShareButton className="btn" url={this.state.tripSelectionUrl} title={"Please select trip options for " + this.state.destination}>
+                        <WhatsappIcon size={40} round={true} />
+                    </WhatsappShareButton>
+                    <h6>WhatsApp</h6>
+                </div>
+            </div>
+            <br /><br /><br />
+            <div style={{ display: "inline-flex" }}>
+                <input id="tripSelectionUrl" className="form-control inputPlace" defaultValue={this.state.tripSelectionUrl} readOnly />
+                <button className="btn btn-dark" onClick={this.CopyLinkSelect}> Copy Link</button>
+            </div>
         </div>
-        <br />
-        <div>
-            <label>Link to View Options</label>
-            <input className="inputPlace" defaultValue={'http://localhost:3000' + this.state.tripOptionsUrl} readOnly />
-            <button className="btn btn-link" onClick={() => this.props.history.push(this.state.tripOptionsUrl)}> Click to See Options</button>
+
+    CopyLinkSelect = () => {
+        const tripSelectionUrl = document.getElementById('tripSelectionUrl');
+        tripSelectionUrl.select();
+        document.execCommand("copy");
+        //alert("Copied the text: " + tripSelectionUrl.value);
+    }
+
+    ShareComponent = () =>
+        <div className="card">
+            <div>
+
+            </div>
         </div>
-    </div>
 
     TripSuccessComponent = () =>
         <div>
             <h4>Trip Successful! Share with friends...</h4>
-            <br />
+            <br /><br />
             <this.DisplayLinks />
         </div>
 
@@ -255,36 +300,44 @@ class Propose extends Component {
 
     render() {
         return (
-            <div className="jumbotron container" style={styles.divHome}>
-                {this.state.activeStep !== 5 ?
-                    <div>
-                        {this.switchComponent(this.state.activeStep)}
-                        <br /><br />
-                        <div className="d-flex justify-content-around">
+            <div className="container">
+                <br />
+                {this.state.activeStep <= 4 ? <div className="progress">
+                    <div ref="progressTrip" className="progress-bar progress-bar-striped bg-dark" role="progressbar" style={{ width: this.state.width + '%' }} aria-valuemin="0" aria-valuemax="100">{this.state.width}%</div>
+                </div> : <div></div>}
+                <div className="jumbotron container" style={styles.divHome}>
+                    {this.state.activeStep !== 5 ?
+                        <div>
+                            {this.switchComponent(this.state.activeStep)}
+                            <br /><br />
+                            <div className="d-flex justify-content-around">
 
-                            <button className="btn btn-light" onClick={() => {
-                                this.setState({ activeStep: this.state.activeStep - 1 })
-                            }} disabled={this.state.activeStep === 0}>Back</button>
+                                <button className="btn btn-light" onClick={() => {
+                                    this.refs.progressTrip.style.width = this.state.width - 20 + '%';
+                                    this.setState({ activeStep: this.state.activeStep - 1, width: this.state.width - 20 })
+                                }} disabled={this.state.activeStep === 0}>Back</button>
 
-                            {this.state.activeStep !== 4 ?
-                                <button className="btn btn-dark" onClick={() => {
-                                    if (this.state.destination !== "" && this.state.activeStep === 0) {
-                                        this.generateGoogleAndAirbnbPlaces();
-                                    }
-                                    this.setState({ activeStep: this.state.activeStep + 1 })
-                                }}>Next</button>
-                                :
-                                <button className="btn btn-dark" onClick={() => {
-                                    this.generateNewTrip();
-                                    this.setState({ activeStep: this.state.activeStep + 1 })
-                                }}>Finish</button>}
+                                {this.state.activeStep !== 4 ?
+                                    <button className="btn btn-dark" onClick={() => {
+                                        if (this.state.destination !== "" && this.state.activeStep === 0) {
+                                            this.generateGoogleAndAirbnbPlaces();
+                                        };
+                                        this.refs.progressTrip.style.width = this.state.width + 20 + '%';
+                                        this.setState({ activeStep: this.state.activeStep + 1, width: this.state.width + 20 })
+                                    }}>Next</button>
+                                    :
+                                    <button className="btn btn-dark" onClick={() => {
+                                        this.generateNewTrip();
+                                        this.setState({ activeStep: this.state.activeStep + 1 })
+                                    }}>Finish</button>}
+                            </div>
                         </div>
-                    </div>
-                    :
-                    <this.TripSuccessComponent />
-                }
-                <br /><br />
-                <h6>Proceed to <Link to="/">Home</Link></h6>
+                        :
+                        <this.TripSuccessComponent />
+                    }
+                    <br /><br />
+                    <h6>Proceed to <Link to="/">Home</Link></h6>
+                </div>
             </div>
         );
     }
