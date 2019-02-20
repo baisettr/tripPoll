24cf752a7e47bfa688dc8c9597234645e599311c
rk <ramakrishna8051@gmail.com>
2019-02-20 10:08:15 -0800

    adding user related services

--- services/user.js
+++ services/user.js
@@ -13,7 +13,8 @@ function userSignUp(userDetails) {
                     console.log("success sign up");
                     const user = { userId: res.data.userId, userOId: res.data._id.$oid };
                     const token = jwt.sign(user, keys.jwtKey, { algorithm: 'HS256', expiresIn: 60 * 60 });
-                    resolve(token);
+                    const userClient = { token, userId: user.userId, userName: res.data.userName };
+                    resolve(userClient);
                 } else {
                     reject("Invalid registration details!");
                 }
@@ -36,8 +37,9 @@ function userSignIn(userDetails) {
                         console.log("success");
                         const user = { userId: userFullDetails.userId, userOId: userFullDetails._id.$oid };
                         const token = jwt.sign(user, keys.jwtKey, { algorithm: 'HS256', expiresIn: 60 * 60 });
-                        console.log(token);
-                        resolve(token);
+                        //console.log(token);
+                        const userClient = { token, userId: user.userId, userName: userFullDetails.userName };
+                        resolve(userClient);
                     } else {
                         reject("Incorrect password. Please try again!");
                     };
@@ -73,7 +75,101 @@ function userDetails(userDetails) {
     });
 }
 
-module.exports = { userSignUp, userSignIn, userDetails };
+function userNewTrip(newTrip, user) {
+    return new Promise((resolve, reject) => {
+        const userOId = user.userOId;
+        const userId = user.userId;
+        const url = 'https://api.mlab.com/api/1/databases/tripo/collections/users?apiKey=' + keys.mlabAPIKey + '&q={"userId":' + userId + '}&f={"userTrips":1}';
+        axios.get(url)
+            .then((res) => {
+                //console.log(res.data[0]);
+                //console.log(newTrip);
+                const userOldTrips = res.data[0].userTrips;
+                const newUserTrips = [...userOldTrips, newTrip];
+                const urlNew = 'https://api.mlab.com/api/1/databases/tripo/collections/users/' + userOId + '?apiKey=' + keys.mlabAPIKey;
+                axios.put(urlNew, { "$set": { "userTrips": newUserTrips } })
+                    .then((res) => {
+                        //console.log(res.data);
+                        resolve(res.data);
+                    })
+            })
+            .catch((err) => {
+                reject("Internal error. Please try again!");
+            });
+    });
+}
+
+function userNewResponse(newResponse, user) {
+    return new Promise((resolve, reject) => {
+        const userOId = user.userOId;
+        const userId = user.userId;
+        const url = 'https://api.mlab.com/api/1/databases/tripo/collections/users?apiKey=' + keys.mlabAPIKey + '&q={"userId":' + userId + '}&f={"userResponses":1}';
+        axios.get(url)
+            .then((res) => {
+                //console.log(res.data);
+                const userOldResponses = res.data[0].userResponses;
+                console.log(userOldResponses);
+                let newUserResponses = [];
+                userOldResponses.forEach((o) => { if (o.tripId !== newResponse.tripId) { newUserResponses.push(o) } });
+                newUserResponses.push(newResponse);
+                console.log(newUserResponses);
+                const urlNew = 'https://api.mlab.com/api/1/databases/tripo/collections/users/' + userOId + '?apiKey=' + keys.mlabAPIKey;
+                axios.put(urlNew, { "$set": { "userResponses": newUserResponses } })
+                    .then((res) => {
+                        //console.log(res.data);
+                        resolve(res.data);
+                    })
+            })
+            .catch((err) => {
+                reject("Internal error. Please try again!");
+            });
+    });
+}
+
+function userTrips(user) {
+    return new Promise((resolve, reject) => {
+        const userId = user.userId;
+        console.log(userId);
+        const url = 'https://api.mlab.com/api/1/databases/tripo/collections/users?apiKey=' + keys.mlabAPIKey + '&q={"userId":' + userId + '}&f={"userTrips":1}';
+        axios.get(url)
+            .then((res) => {
+                //console.log(res.data[0]);
+                const userTrips = res.data[0].userTrips;
+                if (userTrips.length !== 0) {
+                    console.log("success user trips");
+                    resolve(userTrips);
+                } else {
+                    reject("No planned Trips Yet! Try a new one.");
+                };
+            })
+            .catch((err) => {
+                reject("Internal error. Please try again!");
+            });
+    });
+}
+
+function userResponses(user) {
+    return new Promise((resolve, reject) => {
+        const userId = user.userId;
+        const url = 'https://api.mlab.com/api/1/databases/tripo/collections/users?apiKey=' + keys.mlabAPIKey + '&q={"userId":' + userId + '}&f={"userResponses":1}';
+        axios.get(url)
+            .then((res) => {
+                //console.log(res.data[0]);
+                const userResponses = res.data[0].userResponses;
+                if (userResponses.length !== 0) {
+                    console.log("success user responses");
+                    resolve(userResponses);
+                } else {
+                    reject("You have no Trips to respond or check!");
+                };
+            })
+            .catch((err) => {
+                reject("Internal error. Please try again!");
+            });
+    });
+}
+
+module.exports = { userSignUp, userSignIn, userDetails, userNewTrip, userNewResponse, userTrips, userResponses };
 
 
 
